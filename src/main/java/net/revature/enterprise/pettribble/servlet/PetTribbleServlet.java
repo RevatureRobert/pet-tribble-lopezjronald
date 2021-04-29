a0cb9d241eca2953d2ba4b542fcb121b8a8ecb52
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2021-04-29 14:45:51 -0700

    configured the names to get it to work. working on displaying in json format

--- src/main/java/net/revature/enterprise/pettribble/servlet/PetTribbleServlet.java
+++ src/main/java/net/revature/enterprise/pettribble/servlet/PetTribbleServlet.java
@@ -1,18 +1,21 @@
 package net.revature.enterprise.pettribble.servlet;
 
 import com.google.gson.Gson;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.revature.enterprise.pettribble.database.PostgreDatabase;
+import net.revature.enterprise.pettribble.model.PetTribble;
 import net.revature.enterprise.pettribble.service.PetTribbleImpl;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.ArrayList;
 
-@WebServlet(urlPatterns = "/PetRocks")
+@WebServlet(urlPatterns = "/PetTribbles")
 public class PetTribbleServlet extends HttpServlet {
 
 
@@ -28,7 +31,10 @@ public class PetTribbleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.getWriter().println("hello");
+        ArrayList<PetTribble> petTribbles = petTribbleImpl.getAllPetTribbles(connection);
+        request.setAttribute("TRIBBLE_LIST", petTribbles);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/all-tribbles");
+        requestDispatcher.forward(request, response);
 
     }
 
